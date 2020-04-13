c7353de6d32466d5d57e02ec55f45c809998510e
Сухарева Мария <msukhareva@MacBook-Pro-localadmin.local>
2020-04-13 20:44:28 +0300

    Added function for first homework

---
+++ javascript/1-carry_sum/index.js
@@ -0,0 +1,13 @@
+function sum (arg1){
+    let nowSum = arg1;
+
+    function nextSum(arg2){
+        if (arg2 !== undefined){
+            nowSum += arg2;
+            return nextSum;
+        }
+        else return nowSum;
+    }
+
+    return nextSum;
+}
