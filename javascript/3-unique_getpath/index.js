b024c22e6564801681781cc247808252559a2845
Сухарева Мария <msukhareva@MacBook-Pro-localadmin.local>
2020-05-06 09:21:25 +0300

    updated function

--- javascript/3-unique_getpath/index.js
+++ javascript/3-unique_getpath/index.js
@@ -2,24 +2,15 @@ const html = document.documentElement;
 const body = document.body;
 
 const uniqueEl = (selector) => {
-    const result = html.querySelectorAll(selector).length > 1 ? false : true;
-    return result;
+    return html.querySelectorAll(selector).length > 1 ? false : true;
 }
 const toDecorId = (elem) => {
     return `#${elem.id}`
 }
 const toDecorClass = (elem) => {
-    if (hasSameChildren(elem)) {
-        const index = getIndexChild(elem);
-        return `.${Array.from(elem.classList).join('.')}:nth-child(${index})`
-    }
     return `.${Array.from(elem.classList).join('.')}`
 }
 const toDecorTag = (elem) => {
-    if (hasSameChildren(elem)) {
-        const index = getIndexChild(tagName);
-        return `${elem.tagName.toLowerCase()}:nth-child(${index})`
-    }
     return elem.tagName.toLowerCase();
 }
 const toDecorElemPath = (elems) => {
@@ -37,22 +28,6 @@ const fullPath = (from, to) => {
     }
     return toDecorElemPath(resultPath);
 }
-const getIndexChild = (elem) => {
-    // const index = Array.from(elem.parentElement.children).indexOf(elem);
-    const result = Array.from(elem.parentElement.children).filter(item => {
-        return item == elem;
-    })
-    console.log(result, 'result')
-    const index = result.indexOf(elem)
-    return index;
-}
-const hasSameChildren = (elem) => {
-    result = Array.from(elem.parentElement.children).filter(item => {
-        return item == elem;
-    })
-    return result.length !== 0 ? true : false;
-}
-
 
 const getPath = (elem) => {
     if (elem) {
@@ -65,11 +40,9 @@ const getPath = (elem) => {
         if (uniqueEl(elem.tagName)) {
             return toDecorTag(elem)
         }
-        if (uniqueEl(fullPath(elem, body))) {
-            return fullPath(elem, body);
-        }
-
         return fullPath(elem, body);
 
-    } else return "Current HTML don't have this element"
+    } else return "Document don't have this element"
 }
+
+module.exports = {uniqueEl, toDecorId};
